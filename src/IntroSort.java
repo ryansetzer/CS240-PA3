4fc3b8a9ddeab5a76ecee6e6ec0975bffd6ac0ee
Ryan Setzer <56447440+ryansetzer@users.noreply.github.com>
2022-03-25 02:09:46 -0400

    Init Commit

---
+++ src/IntroSort.java
@@ -0,0 +1,14 @@
+/**
+ * Introsort class.
+ */
+public class IntroSort {
+
+  /**
+   * Sort the provided items using the introsort algorithm.
+   */
+  public static <T extends Comparable<T>> void introSort(T[] items) {
+   
+  }
+
+
+}
---
+++ src/MergeSortsImproved.java
@@ -0,0 +1,36 @@
+
+/**
+ * Improved MergeSort class.
+ */
+
+public class MergeSortsImproved {
+
+  /**
+   * Merge sort the provided array using an improved merge operation.
+   */
+  public static <T extends Comparable<T>> void mergeSort1(T[] items) {
+
+  }
+
+
+  /**
+   * Merge sort the provided array by using an improved merge operation and
+   * switching to insertion sort for small sub-arrays.
+   */
+  public static <T extends Comparable<T>> void mergeSort2(T[] items) {
+
+  }
+
+  /**
+   * Merge sort the provided sub-array using our improved merge sort. This is the
+   * fallback method used by introspective sort.
+   */
+  public static <T extends Comparable<T>> void mergeSort2(T[] items, int start, int end) {
+
+    // You will need to call the insertion sort method from BasicSorts.java:
+    //   insertionSort(T[] items, int start, int end)
+
+
+  }
+
+}
