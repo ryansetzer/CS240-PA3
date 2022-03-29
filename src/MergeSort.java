811f3808ffb161f20c65ca125e1142512c5ca04b
Ryan Setzer <ryanjsetzer@gmail.com>
2022-03-29 13:46:16 -0400

    Working Merge Sort 1

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,8 +1,8 @@
 /*
  * OpenDSA Project Distributed under the MIT License
- * 
+ *
  * Copyright (c) 2011-2016 - Ville Karavirta and Cliff Shaffer
- * 
+ *
  * Modifications by Nathan Sprague 2016.
  */
 
@@ -16,33 +16,26 @@ public class MergeSort {
    */
   @SuppressWarnings("unchecked")
   public static <T extends Comparable<T>> void mergeSort(T[] items) {
-
-    // Unfortunately, it is not possible to create an array of T's, this is the
-    // workaround.
+    // Unfortunately, it is not possible to create an array of T's, this is the workaround.
     T[] temp = (T[]) new Comparable[items.length];
-
     mergeSort(items, temp, 0, items.length - 1);
   }
 
   /**
    * Recursive helper method for the merge sort algorithm.
-   * 
+   *
    * @param items The array to sort
-   * @param temp Temporary array for merge operation
-   * @param left Index of the left end of the region to sort
+   * @param temp  Temporary array for merge operation
+   * @param left  Index of the left end of the region to sort
    * @param right Index of the right end of the region to sort.
    */
-  private static <T extends Comparable<T>> void mergeSort(T[] items, T[] temp, int left,
-      int right) {
+  private static <T extends Comparable<T>> void mergeSort(T[] items, T[] temp, int left, int right) {
     if (left >= right) {
       return; // Region has one record
     }
-
     int mid = (left + right) / 2; // Select midpoint
-
     mergeSort(items, temp, left, mid); // Mergesort first half
     mergeSort(items, temp, mid + 1, right); // Mergesort second half
-
     merge(items, temp, left, mid, right);
 
   }
@@ -50,10 +43,9 @@ public class MergeSort {
   /**
    * Merge two sorted sub-arrays.
    */
-  private static <T extends Comparable<T>> void merge(T[] items, T[] temp, int left, int mid,
-      int right) {
+  private static <T extends Comparable<T>> void merge(T[] items, T[] temp, int left, int mid, int right) {
 
-    for (int i = left; i <= right; i++) {
+    for (int i = left; i <= right ; i++) {
       temp[i] = items[i];// Copy subarray to temp
     }
 
@@ -71,6 +63,4 @@ public class MergeSort {
       }
     }
   }
-
-
 }
--- src/MergeSortsImproved.java
+++ src/MergeSortsImproved.java
@@ -8,11 +8,53 @@ public class MergeSortsImproved {
   /**
    * Merge sort the provided array using an improved merge operation.
    */
+  @SuppressWarnings("unchecked")
   public static <T extends Comparable<T>> void mergeSort1(T[] items) {
-    if (items.length <= 1) return;
-    T[] L1 = half of the items from inlist;
-    T[] L2 = other half of the items from inlist;
-    return merge(mergesort(L1), mergesort(L2));
+    // Unfortunately, it is not possible to create an array of T's, this is the workaround.
+    T[] temp = (T[]) new Comparable[items.length];
+    mergeSort(items, temp, 0, items.length - 1);
+  }
+
+  /**
+   * Recursive helper method for the merge sort algorithm.
+   *
+   * @param items The array to sort
+   * @param temp  Temporary array for merge operation
+   * @param left  Index of the left end of the region to sort
+   * @param right Index of the right end of the region to sort.
+   */
+  private static <T extends Comparable<T>> void mergeSort(T[] items, T[] temp, int left, int right) {
+    if (left >= right) {
+      return; // Region has one record
+    }
+
+    int mid = (left + right) / 2; // Select midpoint
+    mergeSort(items, temp, left, mid); // Mergesort first half
+    mergeSort(items, temp, mid + 1, right); // Mergesort second half
+    merge(items, temp, left, mid, right);
+
+  }
+
+  private static <T extends Comparable<T>> void merge(T[] items, T[] temp, int left, int mid, int right) {
+
+    for (int i = left; i <= mid; i++) {
+      temp[i] = items[i];// Copy sub-array to temp
+    }
+
+    int i1 = left;
+    int i2 = mid + 1;
+
+    for (int curr = left; curr <= right; curr++) {
+      if (i1 == mid + 1) { // Left sub-array exhausted
+        items[curr] = items[i2++];
+      } else if (i2 > right) { // Right sub-array exhausted
+        items[curr] = temp[i1++];
+      } else if (temp[i1].compareTo(items[i2]) <= 0) { // Get smaller value
+        items[curr] = temp[i1++];
+      } else {
+        items[curr] = items[i2++];
+      }
+    }
   }
 
 
@@ -21,7 +63,7 @@ public class MergeSortsImproved {
    * switching to insertion sort for small sub-arrays.
    */
   public static <T extends Comparable<T>> void mergeSort2(T[] items) {
-
+    mergeSort2(items, 0, items.length - 1);
   }
 
   /**
@@ -34,6 +76,28 @@ public class MergeSortsImproved {
     //   insertionSort(T[] items, int start, int end)
 
 
+  }
+
+  private static <T extends Comparable<T>> void merge2(T[] items, T[] temp, int left, int mid, int right) {
+
+    for (int i = left; i <= mid; i++) {
+      temp[i] = items[i];// Copy sub-array to temp
+    }
+
+    int i1 = left;
+    int i2 = mid + 1;
+
+    for (int curr = left; curr <= right; curr++) {
+      if (i1 == mid + 1) { // Left sub-array exhausted
+        items[curr] = items[i2++];
+      } else if (i2 > right) { // Right sub-array exhausted
+        items[curr] = temp[i1++];
+      } else if (temp[i1].compareTo(items[i2]) <= 0) { // Get smaller value
+        items[curr] = temp[i1++];
+      } else {
+        items[curr] = items[i2++];
+      }
+    }
   }
 
 }
